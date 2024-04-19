0c1e9a2fa99bbc7be4ed317763b692b1a64307b7
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-04-18 21:56:44 -0300

    feat: creating route's index file

---
+++ src/pages/routes/index.jsx
@@ -0,0 +1,9 @@
+import styles from "../../styles/Routes.module.css";
+
+export default function Routes() {
+  return (
+    <div className={styles.container}>
+      <h1>Index</h1>
+    </div>
+  );
+}
