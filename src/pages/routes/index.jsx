f865564f1b52448c66a9b5f05f2d715255bae424
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-04-18 22:20:23 -0300

    feat: using "programmable" routing system

--- src/pages/routes/index.jsx
+++ src/pages/routes/index.jsx
@@ -1,7 +1,23 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
 import styles from "../../styles/Routes.module.css";
 
 export default function Routes() {
+  const router = useRouter();
+
+  const navegacaoSimples = (url) => {
+    router.push(url);
+  };
+
+  const navegacaoComParams = () => {
+    router.push({
+      pathname: "/routes/params",
+      query: {
+        id: 12,
+        nome: "Wagner Morais",
+      },
+    });
+  };
   return (
     <div className={styles.container}>
       <h1>Index</h1>
@@ -13,6 +29,13 @@ export default function Routes() {
       </div>
       <div>
         <Link href="/routes/1/Bruna">Params</Link>
+      </div>
+      <div className={styles.buttonBox}>
+        <button onClick={() => router.push("/routes/123/Pedro")}>Buscar</button>
+        <button onClick={() => navegacaoSimples("/routes/1/Bruna")}>
+          Bruna
+        </button>
+        <button onClick={() => navegacaoComParams()}>Params</button>
       </div>
     </div>
   );
