a72a60ac6e386fb656f439304ed77f5c8cdb3fb7
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-04-24 12:15:10 -0300

    feat: adding typescript props

--- projects/01 - Porta Premiada/doors/src/components/Door.tsx
+++ projects/01 - Porta Premiada/doors/src/components/Door.tsx
@@ -1,15 +1,17 @@
+import { DoorProps } from "../types/DoorProps";
 import styles from "../styles/Door.module.css";
 
-export default function Door(props) {
+export default function Door(props: DoorProps) {
+  const { door } = props;
   return (
     <div className={styles.container}>
       <div
         className={`${styles.structure} ${
-          props.isDoorSelected ? styles.doorSelected : ""
-        }`}
+          door.isSelected ? styles.doorSelected : ""
+        }}`}
       >
         <div className={styles.door}>
-          <div className={styles.doorNumber}>3</div>
+          <div className={styles.doorNumber}>{door.doorNumber}</div>
           <div className={styles.doorHandle}></div>
         </div>
       </div>
