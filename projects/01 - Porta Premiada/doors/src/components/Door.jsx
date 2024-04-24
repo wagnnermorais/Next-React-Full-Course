518c17fa5c587f9f6e4a6c0839af2c1b6d8bb68f
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-04-24 11:27:22 -0300

    feat: finishing door component structure

--- projects/01 - Porta Premiada/doors/src/components/Door.jsx
+++ projects/01 - Porta Premiada/doors/src/components/Door.jsx
@@ -2,8 +2,18 @@ import styles from "../styles/Door.module.css";
 
 export default function Door(props) {
   return (
-    <div className={styles.door}>
-      <div className={styles.doorNumber}>3</div>
+    <div className={styles.container}>
+      <div
+        className={`${styles.structure} ${
+          props.isDoorSelected ? styles.doorSelected : ""
+        }`}
+      >
+        <div className={styles.door}>
+          <div className={styles.doorNumber}>3</div>
+          <div className={styles.doorHandle}></div>
+        </div>
+      </div>
+      <div className={styles.floor}></div>
     </div>
   );
 }
