0fe58e7b3ecc7862447700ff3b456e112b32b2e2
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-04-24 10:38:44 -0300

    feat: adding door component

--- projects/01 - Porta Premiada/doors/src/pages/index.jsx
+++ projects/01 - Porta Premiada/doors/src/pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Gift from "@/components/Gift";
+import Door from "@/components/Door";
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Gift />
+      <Door />
     </>
   );
 }
