1574d80d7fb495dbf91223de2bb945bf9857ed0a
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-04-24 16:49:16 -0300

    feat: adding highlight door handler event

--- projects/01 - Porta Premiada/doors/src/components/Door.tsx
+++ projects/01 - Porta Premiada/doors/src/components/Door.tsx
@@ -2,13 +2,18 @@ import { DoorProps } from "../types/DoorProps";
 import styles from "../styles/Door.module.css";
 
 export default function Door(props: DoorProps) {
-  const { door } = props;
+  const door = props.value;
+
+  const handleChangeSelectedDoor = (e) => {
+    props.onChange(door.changeSelected());
+  };
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={handleChangeSelectedDoor}>
       <div
         className={`${styles.structure} ${
           door.isSelected ? styles.doorSelected : ""
-        }}`}
+        }`}
       >
         <div className={styles.door}>
           <div className={styles.doorNumber}>{door.doorNumber}</div>
