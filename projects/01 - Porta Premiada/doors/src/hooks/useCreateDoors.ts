166f178ef4cdc2d0fae4dcc8ef996edeca9ca960
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-04-30 16:15:01 -0300

    refac: changing param name

--- projects/01 - Porta Premiada/doors/src/hooks/useCreateDoors.ts
+++ projects/01 - Porta Premiada/doors/src/hooks/useCreateDoors.ts
@@ -1,9 +1,9 @@
 import DoorModel from "../model/DoorModel";
 
-export function useCreateDoors(quantity: number, doorSelected: number): DoorModel[] {
+export function useCreateDoors(quantity: number, giftedDoor: number): DoorModel[] {
   return Array.from({ length: quantity }, (_, i) => {
     const number = i + 1;
-    const hasGift = number === doorSelected
+    const hasGift = number === giftedDoor
     return new DoorModel(number, hasGift)
   })
 }
