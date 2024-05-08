42ba55877cfaaa60ccf583d51a590288c59d0357
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-07 21:47:38 -0300

    feat: standardizing id values and creating new questions

--- projects/02 - NEXT Quiz/src/pages/api/db/database.ts
+++ projects/02 - NEXT Quiz/src/pages/api/db/database.ts
@@ -2,17 +2,29 @@ import QuestionModel from "../../../../models/question";
 import AnswerModel from "../../../../models/answer";
 
 const questions: QuestionModel[] = [
-  new QuestionModel(777, "Qual animal transmite a doença de Chagas?", [
+  new QuestionModel(1, "Qual animal transmite a doença de Chagas?", [
     AnswerModel.wrongAnswer("Abelha"),
     AnswerModel.wrongAnswer("Barata"),
     AnswerModel.wrongAnswer("Pulga"),
     AnswerModel.rightAnswer("Barbeiro"),
   ]),
-  new QuestionModel(666, "Qual fruto é conhecido no Norte e Nordeste como 'jerimum'?", [
+  new QuestionModel(2, "Qual fruto é conhecido no Norte e Nordeste como 'jerimum'?", [
     AnswerModel.wrongAnswer("Abelha"),
     AnswerModel.wrongAnswer("Barata"),
     AnswerModel.wrongAnswer("Pulga"),
     AnswerModel.rightAnswer("Barbeiro"),
+  ]),
+  new QuestionModel(3, "Qual é o coletivo de cães?", [
+    AnswerModel.wrongAnswer("Manada"),
+    AnswerModel.wrongAnswer("Alcateia"),
+    AnswerModel.wrongAnswer("Rebanho"),
+    AnswerModel.rightAnswer("Matilha"),
+  ]),
+  new QuestionModel(4, "Qual fruto é conhecido no Norte e Nordeste como 'jerimum'?", [
+    AnswerModel.wrongAnswer("Equilátero"),
+    AnswerModel.wrongAnswer("Isóceles"),
+    AnswerModel.wrongAnswer("Trapézio"),
+    AnswerModel.rightAnswer("Escaleno"),
   ]),
 ]
 
