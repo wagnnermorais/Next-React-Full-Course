6831a7acf76162402bdc2c30d041062e83968961
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-07 21:48:03 -0300

    feat: creating questions api

--- projects/02 - NEXT Quiz/src/pages/api/questions/[id].ts
+++ projects/02 - NEXT Quiz/src/pages/api/questions/[id].ts
@@ -1,7 +1,14 @@
 import database from "../db/database"
 
 export default function handler(req, res) {
-  res.status(200).json(
-    database[0].toObject()
-  )
+  const selectedID = +req.query.id
+  const questions = database.filter(question => question.id === selectedID)
+
+  if (questions.length === 1) {
+    const selectedQuestion = questions[0]
+    res.status(200).json(selectedQuestion.toObject())
+    return
+  }
+
+  return res.status(204).send()
 }
