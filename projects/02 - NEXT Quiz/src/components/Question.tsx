3b3dbef83a13300a3c5a0183921955faaafa4337
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-11 16:19:27 -0300

    feat: inserting statement component into question component

--- projects/02 - NEXT Quiz/src/components/Question.tsx
+++ projects/02 - NEXT Quiz/src/components/Question.tsx
@@ -1,7 +1,12 @@
-export default function Question(props) {
+import { QuestionProps } from "../types/QuestionProps";
+import styles from "../styles/Question.module.css";
+import Statement from "./Statement";
+
+export default function Question(props: QuestionProps) {
+  const question = props.value;
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className={styles.questionContainer}>
+      <Statement text={question.statement} />
     </div>
   );
 }
