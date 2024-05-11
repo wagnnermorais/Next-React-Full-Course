ce196813ffcb76dd1ad132a808dbe84131afc645
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-10 22:24:21 -0300

    feat: sorting answers in a random order using class method

--- projects/02 - NEXT Quiz/src/pages/api/questions/[id].ts
+++ projects/02 - NEXT Quiz/src/pages/api/questions/[id].ts
@@ -5,7 +5,7 @@ export default function handler(req, res) {
   const questions = database.filter(question => question.id === selectedID)
 
   if (questions.length === 1) {
-    const selectedQuestion = questions[0]
+    const selectedQuestion = questions[0].sortAnswers()
     res.status(200).json(selectedQuestion.toObject())
     return
   }
