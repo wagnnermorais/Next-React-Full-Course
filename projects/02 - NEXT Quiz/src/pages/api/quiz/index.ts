b9e3df3f811808b84721a584d96ea3abd54b4459
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-10 22:23:36 -0300

    feat: sorting ids

--- projects/02 - NEXT Quiz/src/pages/api/quiz/index.ts
+++ projects/02 - NEXT Quiz/src/pages/api/quiz/index.ts
@@ -1,7 +1,7 @@
+import useSorteQuestions from "../../../hooks/useSortQuestions"
 import database from "../db/database"
 
 export default function handler(req, res) {
-  res.status(200).json(database.map((question) => (
-    question.id
-  )))
+  const ids = database.map(question => question.id)
+  res.status(200).json(useSorteQuestions(ids))
 }
