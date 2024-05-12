c70536c73c4faf66b7460a409a9b2fd0bbf60685
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-12 17:27:16 -0300

    feat: mapping answers in question component

--- projects/02 - NEXT Quiz/src/components/Question.tsx
+++ projects/02 - NEXT Quiz/src/components/Question.tsx
@@ -1,11 +1,15 @@
 import { QuestionProps } from "../types/QuestionProps";
 import styles from "../styles/Question.module.css";
 import Statement from "./Statement";
+import Answer from "./Answer";
 
 export default function Question(props: QuestionProps) {
   const question = props.value;
   return (
     <div className={styles.questionContainer}>
+      {question.answer.map((answer, index) => (
+        <Answer value={answer} index={index} letter="A" letterColor="#F2C866" />
+      ))}
       <Statement text={question.statement} />
     </div>
   );
