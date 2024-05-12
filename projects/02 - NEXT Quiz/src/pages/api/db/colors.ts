f7363be66dcd407457faf4ef22679398e3fa15d1
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-12 18:07:28 -0300

    feat: creating background database

---
+++ projects/02 - NEXT Quiz/src/pages/api/db/colors.ts
@@ -0,0 +1,8 @@
+const letters = [
+  { value: "A", background: "#F2C866" },
+  { value: "B", background: "#F266BA" },
+  { value: "C", background: "#85D4F2" },
+  { value: "D", background: "#BCE596" },
+];
+
+export default letters;
