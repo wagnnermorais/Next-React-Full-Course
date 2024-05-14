bd2ef5d32cdec56c2b53a31773d267313bad2119
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-14 19:43:08 -0300

    feat: adding timer from new prop

--- projects/02 - NEXT Quiz/src/pages/index.tsx
+++ projects/02 - NEXT Quiz/src/pages/index.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import Head from "next/head";
 import Question from "../components/Question";
-import styles from "../styles/Home.module.css";
+import Button from "../components/Button";
 import QuestionModel from "../../models/question";
 import AnswerModel from "../../models/answer";
+import styles from "../styles/Home.module.css";
 
 const mockQuestion = new QuestionModel(1, "Melhor cor?", [
   AnswerModel.wrongAnswer("Green"),
@@ -31,7 +32,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Question value={question} onResponse={onResponse} timeout={timeout} />
+      <Question
+        timeForAnswer={5}
+        value={question}
+        onResponse={onResponse}
+        timeout={timeout}
+      />
+      <Button text="Next question" href="/result" />
     </div>
   );
 }
