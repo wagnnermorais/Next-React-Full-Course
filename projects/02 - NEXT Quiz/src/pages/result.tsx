fe945f64aded61c2aa01bffa74f2dd990d369e2d
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-14 19:41:06 -0300

    feat: creating result page

---
+++ projects/02 - NEXT Quiz/src/pages/result.tsx
@@ -0,0 +1,10 @@
+import { ResultProps } from "../types/ResultProps";
+import styles from "../styles/Result.module.css";
+
+export default function Result(props: ResultProps) {
+  return (
+    <div>
+      <h1>Resultado</h1>
+    </div>
+  );
+}
