1adbbd0de623dd536fc363099a4ebfb891cb06fd
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-18 19:06:44 -0300

    refac: creating new colors colors object

--- projects/02 - NEXT Quiz/src/pages/api/db/colors.ts
+++ projects/02 - NEXT Quiz/src/pages/api/db/colors.ts
@@ -1,8 +1,25 @@
-const letters = [
+export const letters = [
   { value: "A", background: "#F2C866" },
   { value: "B", background: "#F266BA" },
   { value: "C", background: "#85D4F2" },
   { value: "D", background: "#BCE596" },
 ];
 
-export default letters;
+export const averages = [
+  {
+    average: "0-25%",
+    color: "#8B0000"
+  },
+  {
+    average: "25-49%",
+    color: "#db190b"
+  },
+  {
+    average: "50-75%",
+    color: "#9CD2A4"
+  },
+  {
+    average: "75-100%",
+    color: "#02c952"
+  }
+]; 
