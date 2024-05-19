071a3fa868e765be1595482da1374f87c2072608
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-19 19:24:41 -0300

    style: styling layout component

--- projects/03 - ADMIN Template/src/components/template/Layout.tsx
+++ projects/03 - ADMIN Template/src/components/template/Layout.tsx
@@ -5,10 +5,12 @@ import { LayoutProps } from "../../types/LayoutProps";
 
 export default function Layout(props: LayoutProps) {
   return (
-    <div>
+    <div className="flex w-full min-h-screen dark">
       <Sidebar />
-      <Header title={props.title} subtitle={props.subtitle} />
-      <Content>{props.children}</Content>
+      <div className="flex flex-col w-full p-4 bg-zinc-300 dark:bg-zinc-900 dark:text-white">
+        <Header title={props.title} subtitle={props.subtitle} />
+        <Content>{props.children}</Content>
+      </div>
     </div>
   );
 }
