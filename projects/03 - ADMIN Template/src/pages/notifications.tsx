da23527d834a88cbbacc7f6106518e80d4d5b4d1
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-20 19:56:29 -0300

    feat: creating 2 views

---
+++ projects/03 - ADMIN Template/src/pages/notifications.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/template/Layout";
+
+export default function Notifications() {
+  return (
+    <div>
+      <Layout
+        title="Notifications management"
+        subtitle="Manage your notifications here"
+      >
+        Content
+      </Layout>
+    </div>
+  );
+}
---
+++ projects/03 - ADMIN Template/src/pages/settings.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/template/Layout";
+
+export default function Settingd() {
+  return (
+    <div>
+      <Layout title="Settings" subtitle="Manage your personal settings">
+        Content
+      </Layout>
+    </div>
+  );
+}
