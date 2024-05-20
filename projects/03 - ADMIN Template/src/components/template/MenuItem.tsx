b842938e15512ee0b9b844c091cf49ca46798f7e
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-20 19:55:10 -0300

    feat: applying styling in menuitem

--- projects/03 - ADMIN Template/src/components/template/MenuItem.tsx
+++ projects/03 - ADMIN Template/src/components/template/MenuItem.tsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
 import { MenuItemProps } from "@/types/MenuItemProps";
 
 export default function MenuItem(props: MenuItemProps) {
   return (
     <div>
-      <li className="">
-        {props.icon} {props.text}
+      <li className="hover:bg-gray-200">
+        <Link href={props.url}>
+          <p className="flex flex-col justify-center items-center w-full h-20">
+            {props.icon}{" "}
+            <span className="text-sm font-light text-gray-600">
+              {props.text}
+            </span>
+          </p>
+        </Link>
       </li>
     </div>
   );
