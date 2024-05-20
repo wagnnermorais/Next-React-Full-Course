d739b0bc69bff38ac15ab622c206a229c455322b
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-20 19:55:27 -0300

    feat: adding logout icon and new classes

--- projects/03 - ADMIN Template/src/components/template/Sidebar.tsx
+++ projects/03 - ADMIN Template/src/components/template/Sidebar.tsx
@@ -1,13 +1,20 @@
-import { BellIcon, HomeIcon, SettingsIcon } from "../icons";
+import { BellIcon, HomeIcon, LogoutIcon, SettingsIcon } from "../icons";
+import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function Sidebar() {
   return (
-    <aside>
-      <ul>
+    <aside className="flex flex-col">
+      <div className="flex flex-col items-center justify-center w-full h-20 bg-gradient-to-r from-indigo-500 to-purple-800">
+        <Logo />
+      </div>
+      <ul className="flex-grow px-2">
         <MenuItem url="/" text="Home" icon={HomeIcon} />
         <MenuItem url="/settings" text="Settings" icon={SettingsIcon} />
         <MenuItem url="/notifications" text="Notifications" icon={BellIcon} />
+      </ul>
+      <ul className="py-4 px-2">
+        <MenuItem url="/" text="Logout" icon={LogoutIcon} />
       </ul>
     </aside>
   );
