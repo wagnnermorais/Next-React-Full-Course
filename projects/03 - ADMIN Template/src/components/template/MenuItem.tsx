b7e57fd88b4e3651f2abad6cc5f3d22253631028
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-21 23:26:01 -0300

    feat: applying className prop

--- projects/03 - ADMIN Template/src/components/template/MenuItem.tsx
+++ projects/03 - ADMIN Template/src/components/template/MenuItem.tsx
@@ -4,15 +4,26 @@ import { MenuItemProps } from "@/types/MenuItemProps";
 export default function MenuItem(props: MenuItemProps) {
   return (
     <div>
-      <li className="hover:bg-gray-200">
-        <Link href={props.url}>
-          <p className="flex flex-col justify-center items-center w-full h-20">
-            {props.icon}{" "}
+      <li className="hover:bg-gray-200" onClick={props.onClick}>
+        {props.url ? (
+          <Link href={props.url}>
+            <p
+              className={`flex flex-col justify-center items-center w-full h-20 text-gray-600 ${props.className}`}
+            >
+              {props.icon}{" "}
+              <span className="text-sm font-light">{props.text}</span>
+            </p>
+          </Link>
+        ) : (
+          <p
+            className={`flex flex-col justify-center items-center w-full h-20 cursor-pointer ${props.className}`}
+          >
+            {props.icon}
             <span className="text-sm font-light text-gray-600">
               {props.text}
             </span>
           </p>
-        </Link>
+        )}
       </li>
     </div>
   );
