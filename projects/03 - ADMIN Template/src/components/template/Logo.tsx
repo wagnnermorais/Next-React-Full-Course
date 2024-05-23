635391f71177e9027dde96765a3ef54fde7c279f
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-22 21:11:11 -0300

    refac: removing dots logo and adding generic one

--- projects/03 - ADMIN Template/src/components/template/Logo.tsx
+++ projects/03 - ADMIN Template/src/components/template/Logo.tsx
@@ -1,9 +1,9 @@
 export default function Logo() {
   return (
-    <div className="flex items-center justify-center gap-1.5 w-[65%] h-[80%] rounded-full bg-white">
-      <div className="h-3 w-3 rounded-full bg-red-600" />
-      <div className="h-3 w-3 rounded-full bg-yellow-500" />
-      <div className="h-3 w-3 rounded-full bg-green-600" />
-    </div>
+    <img
+      src="/logo.png"
+      alt="Project logo"
+      className="w-full h-full cursor-pointer"
+    />
   );
 }
