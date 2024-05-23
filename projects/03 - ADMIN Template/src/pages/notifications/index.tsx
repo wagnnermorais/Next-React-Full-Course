72f2890b6b91366235edab8675e6a0bdce5240a6
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-22 23:27:57 -0300

    refac: setting index pages and adding theme context

--- projects/03 - ADMIN Template/src/pages/notifications/index.tsx
+++ projects/03 - ADMIN Template/src/pages/notifications/index.tsx
@@ -1,13 +1,15 @@
 import Layout from "@/components/template/Layout";
+import useAppData from "@/hooks/useAppData";
 
 export default function Notifications() {
+  const { handleToggleTheme } = useAppData();
   return (
     <div>
       <Layout
         title="Notifications management"
         subtitle="Manage your notifications here"
       >
-        Content
+        <button onClick={handleToggleTheme}>Click</button>
       </Layout>
     </div>
   );
