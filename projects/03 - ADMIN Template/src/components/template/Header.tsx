ebfdea4e9dd04c2ea5accbaee774243ed1939d28
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-23 20:34:58 -0300

    feat: adding theme toggler

--- projects/03 - ADMIN Template/src/components/template/Header.tsx
+++ projects/03 - ADMIN Template/src/components/template/Header.tsx
@@ -1,10 +1,16 @@
-import { HeaderProps } from "../../types/HeaderProps";
 import Title from "./Title";
+import ThemeToggler from "./ThemeToggler";
+import useAppData from "@/hooks/useAppData";
+import { HeaderProps } from "../../types/HeaderProps";
 
 export default function Header(props: HeaderProps) {
+  const { theme, handleToggleTheme } = useAppData();
   return (
-    <header>
+    <header className="flex">
       <Title title={props.title} subtitle={props.subtitle} />
+      <div className="flex flex-grow justify-end">
+        <ThemeToggler theme={theme} handleToggleTheme={handleToggleTheme} />
+      </div>
     </header>
   );
 }
