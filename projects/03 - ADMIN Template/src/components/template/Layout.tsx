591dd1a19a9a311904a846b273508f4d3561b125
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-22 23:28:12 -0300

    feat: adding theme context

--- projects/03 - ADMIN Template/src/components/template/Layout.tsx
+++ projects/03 - ADMIN Template/src/components/template/Layout.tsx
@@ -1,11 +1,13 @@
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 import Content from "./Content";
+import useAppData from "@/hooks/useAppData";
 import { LayoutProps } from "../../types/LayoutProps";
 
 export default function Layout(props: LayoutProps) {
+  const { theme, handleToggleTheme } = useAppData();
   return (
-    <div className="flex w-full min-h-screen dark">
+    <div className={`${theme} flex w-full min-h-screen`}>
       <Sidebar />
       <div className="flex flex-col w-full p-4 bg-zinc-300 dark:bg-zinc-800 dark:text-white">
         <Header title={props.title} subtitle={props.subtitle} />
