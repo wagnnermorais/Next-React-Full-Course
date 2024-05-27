ec8444e979dbb7b95d10d261a812568e3a864cd1
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-26 21:02:54 -0300

    style: styling input

--- projects/03 - ADMIN Template/src/components/auth/AuthInput.tsx
+++ projects/03 - ADMIN Template/src/components/auth/AuthInput.tsx
@@ -8,6 +8,7 @@ export default function AuthInput(props: AuthInputProps) {
         type={props.type ?? "text"}
         value={props.value}
         onChange={(e) => props.valueChanged?.(e.target.value)}
+        className="py-2 px-4 text-base border border-zinc-400 rounded-sm ease duration-300 outline-none focus:border-blue-500 hover:bg-gray-100"
       />
     </div>
   );
