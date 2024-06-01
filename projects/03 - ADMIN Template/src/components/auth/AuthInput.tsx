f64b300482096fa955bce98e779fe36134465470
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-05-31 21:38:31 -0300

    style: adding floating label to auth input

--- projects/03 - ADMIN Template/src/components/auth/AuthInput.tsx
+++ projects/03 - ADMIN Template/src/components/auth/AuthInput.tsx
@@ -2,14 +2,17 @@ import { AuthInputProps } from "@/types/AuthInputProps";
 
 export default function AuthInput(props: AuthInputProps) {
   return props.render ? null : (
-    <div className="flex flex-col">
-      <label>{props.label}</label>
+    <div className="flex flex-col relative">
       <input
         type={props.type ?? "text"}
         value={props.value}
         onChange={(e) => props.valueChanged?.(e.target.value)}
-        className="py-2 px-4 text-base border border-zinc-400 rounded-sm ease duration-300 outline-none focus:border-blue-500 hover:bg-gray-100"
+        className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        placeholder=" "
       />
+      <label className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">
+        {props.label}
+      </label>
     </div>
   );
 }
