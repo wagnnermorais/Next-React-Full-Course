79d39a1d78bb27ed2d7a54f73fc2e814f7fa59bb
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-06-04 21:05:54 -0300

    feat: adding AuthProvider

--- projects/03 - ADMIN Template/src/pages/_app.tsx
+++ projects/03 - ADMIN Template/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from "next/app";
+import { AuthProvider } from "@/context/AuthContext";
 import { AppProvider } from "@/context/AppContext";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AppProvider>
-      <Component {...pageProps} />
-    </AppProvider>
+    <AuthProvider>
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
+    </AuthProvider>
   );
 }
