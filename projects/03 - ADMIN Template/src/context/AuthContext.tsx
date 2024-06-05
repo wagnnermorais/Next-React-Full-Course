8f5ac1752aae84d3774b5ccaf1ab769d62846252
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-06-04 21:05:10 -0300

    feat: creating auth context

---
+++ projects/03 - ADMIN Template/src/context/AuthContext.tsx
@@ -0,0 +1,40 @@
+import { createContext, useState } from "react";
+import { AuthContextProps } from "@/types/AuthContextProps";
+import route from "next/router";
+import firebase from "../firebase/config";
+import User from "@/models/User";
+
+const AuthContext = createContext<AuthContextProps>({});
+
+// async function normalizedUser(firebaseUser: firebase.User): Promise<User> {
+//   const token = await firebaseUser.getIdToken();
+//   return {
+//     uid: firebaseUser.uid,
+//     name: firebaseUser.displayName,
+//     emai: firebaseUser.email,
+//     token: token,
+//     provider: firebaseUser.providerData[0]?.providerId,
+//     image: firebaseUser.photoURL,
+//   };
+// }
+
+export function AuthProvider(props: any) {
+  const [user, setUser] = useState<User>();
+
+  async function handleGoogleLogin() {
+    console.log("Google login");
+    route.push("/");
+  }
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        handleGoogleLogin,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthContext;
