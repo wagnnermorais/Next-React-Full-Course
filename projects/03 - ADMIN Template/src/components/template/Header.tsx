3753397aae12e8bf56fd88a279f51374ee805585
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-06-10 20:51:25 -0300

    feat: adding user avatar icon in header component

--- projects/03 - ADMIN Template/src/components/template/Header.tsx
+++ projects/03 - ADMIN Template/src/components/template/Header.tsx
@@ -2,14 +2,16 @@ import Title from "./Title";
 import ThemeToggler from "./ThemeToggler";
 import useAppData from "@/hooks/useAppData";
 import { HeaderProps } from "../../types/HeaderProps";
+import UserAvatar from "./UserAvatar";
 
 export default function Header(props: HeaderProps) {
   const { theme, handleToggleTheme } = useAppData();
   return (
     <header className="flex">
       <Title title={props.title} subtitle={props.subtitle} />
-      <div className="flex flex-grow justify-end">
+      <div className="flex items-center flex-grow justify-end gap-4">
         <ThemeToggler theme={theme} handleToggleTheme={handleToggleTheme} />
+        <UserAvatar />
       </div>
     </header>
   );
