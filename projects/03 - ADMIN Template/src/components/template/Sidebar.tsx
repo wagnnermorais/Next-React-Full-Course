71eebb4997406b703a11e1a8100cba73929822a4
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-06-24 20:28:07 -0300

    feat: using logout

--- projects/03 - ADMIN Template/src/components/template/Sidebar.tsx
+++ projects/03 - ADMIN Template/src/components/template/Sidebar.tsx
@@ -1,3 +1,4 @@
+import useAuth from "@/hooks/useAuth";
 import {
   BellIcon,
   HomeIcon,
@@ -9,6 +10,7 @@ import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function Sidebar() {
+  const { handleLogout } = useAuth();
   return (
     <aside className="flex flex-col dark:bg-zinc-900 dark:text-blue-300">
       <div className="flex flex-col items-center justify-center w-full h-20 bg-gradient-to-r from-indigo-800 to-purple-500 dark:bg-gradient-to-r dark:from-zinc-800 dark:to-zinc-500-500 dark:border-b-2 dark:border-gray-700">
@@ -32,6 +34,7 @@ export default function Sidebar() {
           dark:hover:bg-zinc-800
           dark:hover:text-gray-200
         `}
+          onClick={handleLogout}
         />
       </ul>
     </aside>
