8b70ad3f7055fd6a3eb987e30713d18dd1fec661
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-06-24 20:27:35 -0300

    feat: creating logout props

--- projects/03 - ADMIN Template/src/types/AuthContextProps.ts
+++ projects/03 - ADMIN Template/src/types/AuthContextProps.ts
@@ -1,6 +1,7 @@
 import User from "@/models/User";
 
 export interface AuthContextProps {
-  user?: User;
+  user?: User | null;
   handleGoogleLogin: () => Promise<void>
+  handleLogout: () => Promise<void>
 }
