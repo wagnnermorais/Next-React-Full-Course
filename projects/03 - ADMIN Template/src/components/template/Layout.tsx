15489344a24ee25b374549d8e4ff5da5f8425d6c
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-06-25 19:50:33 -0300

    feat: adding forceauth into layout component

--- projects/03 - ADMIN Template/src/components/template/Layout.tsx
+++ projects/03 - ADMIN Template/src/components/template/Layout.tsx
@@ -3,16 +3,19 @@ import Header from "./Header";
 import Content from "./Content";
 import useAppData from "@/hooks/useAppData";
 import { LayoutProps } from "../../types/LayoutProps";
+import ForceAuth from "../auth/ForceAuth";
 
 export default function Layout(props: LayoutProps) {
   const { theme, handleToggleTheme } = useAppData();
   return (
-    <div className={`${theme} flex w-full min-h-screen`}>
-      <Sidebar />
-      <div className="flex flex-col w-full p-4 bg-zinc-300 dark:bg-zinc-800 dark:text-white">
-        <Header title={props.title} subtitle={props.subtitle} />
-        <Content>{props.children}</Content>
+    <ForceAuth>
+      <div className={`${theme} flex w-full min-h-screen`}>
+        <Sidebar />
+        <div className="flex flex-col w-full p-4 bg-zinc-300 dark:bg-zinc-800 dark:text-white">
+          <Header title={props.title} subtitle={props.subtitle} />
+          <Content>{props.children}</Content>
+        </div>
       </div>
-    </div>
+    </ForceAuth>
   );
 }
