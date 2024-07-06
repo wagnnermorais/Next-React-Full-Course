0bad9a9856fa1745dc9b4a1091fed7c4b7b2556c
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-07-06 19:31:30 -0300

    style: changing radius size

--- projects/04 - Final Project Register System/src/components/Layout.tsx
+++ projects/04 - Final Project Register System/src/components/Layout.tsx
@@ -3,7 +3,7 @@ import Title from "./Title";
 
 export default function Layout({ title, children }: LayoutProps) {
   return (
-    <div className="flex flex-col w-2/3 bg-white text-gray-800 rounded-sm shadow-md">
+    <div className="flex flex-col w-2/3 bg-white text-gray-800 rounded-md shadow-md">
       <Title>{title}</Title>
       <div className="p-4">{children}</div>
     </div>
