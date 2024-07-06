21d4f166753d2141e992075993f269ebcbe4e327
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-07-06 19:32:00 -0300

    feat: rendering clients informations

--- projects/04 - Final Project Register System/src/pages/index.tsx
+++ projects/04 - Final Project Register System/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { Poppins } from "next/font/google";
 import Layout from "../components/Layout";
+import Table from "../components/Table";
+import Client from "../core/Client";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -7,12 +9,30 @@ const poppins = Poppins({
 });
 
 export default function Home() {
+  const clients = [
+    new Client("Ana", 24, "1"),
+    new Client("Douglas", 15, "2"),
+    new Client("Vitor", 45, "3"),
+    new Client("Mauricio", 38, "4"),
+  ];
+
+  const selectedClient = (client: Client) => {
+    console.log(client.name);
+  };
+  const deletedClient = (client: Client) => {
+    console.log(client.name + " foi excluido");
+  };
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-center ${poppins.className} bg-gradient-to-r from-blue-300 to-purple-300`}
     >
       <Layout title="test">
-        <p>conteudo</p>
+        <Table
+          clients={clients}
+          selectedClient={selectedClient}
+          deletedClient={deletedClient}
+        />
       </Layout>
     </main>
   );
