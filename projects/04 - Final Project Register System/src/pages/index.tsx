6ca2ae3ae87301e701452a4c34cb78ea6b9221b5
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-07-07 18:01:32 -0300

    feat: separating renders and starting initial user edit

--- projects/04 - Final Project Register System/src/pages/index.tsx
+++ projects/04 - Final Project Register System/src/pages/index.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { Poppins } from "next/font/google";
 import Layout from "../components/Layout";
 import Table from "../components/Table";
 import Client from "../core/Client";
+import Button from "../components/Button";
+import Form from "../components/Form";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -9,6 +12,7 @@ const poppins = Poppins({
 });
 
 export default function Home() {
+  const [isVisible, setIsVisible] = useState<"table" | "form">("table");
   const clients = [
     new Client("Ana", 24, "1"),
     new Client("Douglas", 15, "2"),
@@ -23,16 +27,33 @@ export default function Home() {
     console.log(client.name + " foi excluido");
   };
 
+  const saveClient = (client: Client) => {
+    console.log(client);
+  };
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-center ${poppins.className} bg-gradient-to-r from-blue-300 to-purple-300`}
     >
       <Layout title="test">
-        <Table
-          clients={clients}
-          selectedClient={selectedClient}
-          deletedClient={deletedClient}
-        />
+        {isVisible === "table" ? (
+          <>
+            <div className="flex justify-end">
+              <Button onClick={() => setIsVisible("form")}>Novo cliente</Button>
+            </div>
+            <Table
+              clients={clients}
+              selectedClient={selectedClient}
+              deletedClient={deletedClient}
+            />
+          </>
+        ) : (
+          <Form
+            client={clients[0]}
+            isCanceled={() => setIsVisible("table")}
+            isClientChanged={saveClient}
+          />
+        )}
       </Layout>
     </main>
   );
